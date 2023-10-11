e9b3ef77190bb5a7e16f34bd12ce60422ac15736
JoraDeveloper <viktorpilevins@gmail.com>
2023-10-11 20:24:18 +0300

    [add] Создана общая структура компонентов страницы создания

--- src/components/CreateCardContainer/CreateCardContainer.tsx
+++ src/components/CreateCardContainer/CreateCardContainer.tsx
@@ -1,5 +1,18 @@
+import {
+  Container,
+  Title,
+  EditorContainer,
+  Footer,
+} from "./StyleCreateCardContainer";
+
 function CreateCardContainer() {
-  return "CreateCardContainer";
+  return (
+    <Container>
+      <Title>Создание карточки</Title>
+      <EditorContainer />
+      <Footer>&copy; Жорик</Footer>
+    </Container>
+  );
 }
 
 export default CreateCardContainer;
